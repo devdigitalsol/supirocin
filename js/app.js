09faa2cef2fef856b62f80a35868eea447fa1183
sachind3 <desai.sachin45@gmail.com>
2021-08-27 11:56:30 +0530

    wip

--- js/app.js
+++ js/app.js
@@ -1,30 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
   const startScreen = document.getElementById("startScreen");
   const loginScreen = document.getElementById("loginScreen");
   const playBtn = document.getElementById("playBtn");
   const loginForm = document.querySelector(".loginForm");
   const introLayout = document.querySelector(".introLayout");
+  const gameDisplay = document.getElementById("wrapper");
+  let data = [],
+    allObs = [],
+    setTime = 60,
+    setCount = 4,
+    score = 0,
+    imgs = [],
+    counter = 0,
+    winWidth = window.innerWidth,
+    range,
+    bodyBg = "images/sceenBg.jpg",
+    bodImg = new Image();
 
-  window.addEventListener("load", function () {
-    startScreen.classList.add("hide");
-  });
-  let setTime = 60;
-  let setCount = 10;
-  let obsCount = 0;
-  let range;
-  const body = document.body;
-  let bodyBg = "images/sceenBg.jpg";
-  let bodImg = new Image();
   bodImg.addEventListener("load", function () {
     body.style.backgroundImage = "url(" + bodyBg + ")";
   });
   bodImg.src = bodyBg;
 
-  let score = 0;
-  let imgs = [];
-  let counter = 0;
-  let winWidth = window.innerWidth;
-  let gameDisplay = document.getElementById("wrapper");
   let imgUrls = [
     "images/indications/pink-1.png?" + Math.random(),
     "images/indications/pink-1-left.png?" + Math.random(),
@@ -38,26 +36,30 @@ document.addEventListener("DOMContentLoaded", () => {
     imgs[i].src = imgUrls[i];
     incrementCounter();
   }
-  function incrementCounter() {
-    counter++;
-    if (counter === imgUrls.length) {
-      console.log("done");
-      const loginBtn = document.getElementById("loginBtn");
-      loginBtn.addEventListener("click", function () {
-        loginForm.classList.add("hide");
-        introLayout.classList.add("show");
-      });
-    }
-  }
+
+  window.addEventListener("load", function () {
+    startScreen.classList.add("hide");
+    loginScreen.style.opacity = 1;
+
+    loginForm.addEventListener("click", function () {
+      loginForm.classList.add("hide");
+      introLayout.classList.add("show");
+    });
+  });
   playBtn.addEventListener("click", function () {
+    gameDisplay.style.opacity = 1;
     loginScreen.remove();
     init();
   });
 
+  function incrementCounter() {
+    counter++;
+    if (counter === imgUrls.length) {
+      console.log("done");
+    }
+  }
   function init() {
-    let throwSound = new Audio("sound/throw.mp3?" + Math.random());
-    let goodHit = new Audio("sound/goodHit.mp3?" + Math.random());
-    const data = [
+    data = [
       {
         id: 0,
         name: "Impetigo",
@@ -203,10 +205,38 @@ document.addEventListener("DOMContentLoaded", () => {
         img2: imgs[2],
       },
     ];
-    // let generateObs = setTimeout(generateObstacle, 1000);
-
+    let generateObs = setInterval(() => {
+      generateObstacle();
+    }, 2000);
     startTimer();
+    let mousePointer = document.createElement("div");
+    mousePointer.classList.add("mousePointer");
+    body.addEventListener("touchstart", startTouch);
+    body.addEventListener("touchend", removeTouch);
 
+    function startTouch(e) {
+      body.append(mousePointer);
+      body.addEventListener("touchmove", move);
+      move(e);
+    }
+    function removeTouch(e) {
+      body.removeEventListener("touchmove", move);
+      mousePointer.remove();
+    }
+    function move(e) {
+      let dot = document.createElement("div");
+      dot.classList.add("dot");
+      dot.style.left = `${e.touches[0].pageX - 1}px`;
+      dot.style.top = `${e.touches[0].pageY - 1}px`;
+      body.append(dot);
+      setTimeout(() => {
+        dot.remove();
+      }, 150);
+      mousePointer.style.left = e.touches[0].pageX - 1 + "px";
+      mousePointer.style.top = e.touches[0].pageY - 1 + "px";
+      mousePointer.style.cursor = "pointer";
+      isCollapsed(mousePointer);
+    }
     function generateObstacle() {
       let randomX = Math.floor(Math.random() * (winWidth - 160));
       if (randomX < 80) {
@@ -227,59 +257,43 @@ document.addEventListener("DOMContentLoaded", () => {
         createObs.point
       }</div>
       `;
-      if (createObs.status === "bad") {
-        obsCount = obsCount + 1;
-      }
+
       gameDisplay.appendChild(obstacle);
+      allObs.push(obstacle);
 
-      let mousePointer = document.createElement("div");
-      mousePointer.classList.add("mousePointer");
-      body.addEventListener("touchstart", startTouch);
-      body.addEventListener("touchend", removeTouch);
-
-      function startTouch(e) {
-        body.append(mousePointer);
-        body.addEventListener("touchmove", move);
-        move(e);
-      }
-      function removeTouch(e) {
-        body.removeEventListener("touchmove", move);
-        mousePointer.remove();
-      }
-      function move(e) {
-        let dot = document.createElement("div");
-        dot.classList.add("dot");
-        dot.style.left = `${e.touches[0].pageX - 1}px`;
-        dot.style.top = `${e.touches[0].pageY - 1}px`;
-        body.append(dot);
-        setTimeout(() => {
-          dot.remove();
-        }, 150);
-        // throwSound.play();
-
-        mousePointer.style.left = e.touches[0].pageX - 1 + "px";
-        mousePointer.style.top = e.touches[0].pageY - 1 + "px";
-        mousePointer.style.cursor = "pointer";
-        isCollapsed();
-      }
-      function isCollapsed() {
-        var object_1 = mousePointer.getBoundingClientRect();
-        var object_2 = obstacle.getBoundingClientRect();
+      setTimeout(() => {
+        if (gameDisplay.hasChildNodes()) {
+          obstacle.remove();
+        }
+      }, 4000);
+    }
+    function isCollapsed(mousePointer) {
+      var object_1 = mousePointer.getBoundingClientRect();
+      allObs.forEach((item) => {
+        var object_2 = item.getBoundingClientRect();
         if (
           object_1.left < object_2.left + object_2.width &&
           object_1.left + object_1.width > object_2.left &&
           object_1.top < object_2.top + object_2.height &&
           object_1.top + object_1.height > object_2.top
         ) {
-          obstacle.childNodes.forEach((obs) => {
+          item.childNodes.forEach((obs) => {
             if (obs.className === "mainImg") {
               let getPoint = obs.getAttribute("data-point");
               let getStaus = obs.getAttribute("data-status");
               if (getStaus === "bad") {
-                // goodHit.play();
                 score = score + parseInt(getPoint);
               } else {
                 score = score - parseInt(getPoint);
+              }
+              range = (score * 100) / setCount;
+              document.querySelector(".range").style.width = 100 - range + "%";
+              if (range >= 100) {
+                clearInterval(generateObs);
+                clearInterval(timer);
+                document.querySelectorAll(".obstacle").forEach((item) => {
+                  item.remove();
+                });
               }
               obs.remove();
               if (score > 0) {
@@ -302,28 +316,11 @@ document.addEventListener("DOMContentLoaded", () => {
             }
           });
           setTimeout(() => {
-            obstacle.remove();
+            item.remove();
           }, 3000);
-          // console.log(mainImg);
         }
-      }
-      setTimeout(() => {
-        if (gameDisplay.hasChildNodes()) {
-          obstacle.remove();
-        }
-      }, 4000);
-
-      range = (score * 100) / setCount;
-      document.querySelector(".range").style.width = 100 - range + "%";
-      if (range > 99) {
-        clearInterval(generateObs);
-        clearInterval(timer);
-      }
+      });
     }
-    let generateObs = setInterval(() => {
-      generateObstacle();
-    }, 2000);
-
     function startTimer() {
       let count = setTime;
       timer = setInterval(function () {
